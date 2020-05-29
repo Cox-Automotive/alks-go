6638ee5fb4e28fabaa8f6b18a6d9f295d9941e0d
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2020-05-29 10:15:14 -0400

    Implemented create and delete long term key endpoints.

--- iam_ltk.go
+++ iam_ltk.go
@@ -1,6 +1,7 @@
 package alks
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -18,6 +19,41 @@ type LongTermKey struct {
 type GetLongTermKeysResponse struct {
 	BaseResponse
 	LongTermKeys []LongTermKey `json:"longTermKeys"`
+}
+
+// CreateLongTermKey represents the response from API
+type CreateLongTermKey struct {
+	Account             string `json:"account"`
+	Action              string `json:"action"`
+	IAMUserName         string `json:"iamUserName"`
+	IAMUserArn          string `json:"iamUserArn"`
+	AddedIAMUserToGroup bool   `json:"addedIAMUserToGroup"`
+	PartialError        bool   `json:"partialError"`
+}
+
+// LongTermKeyRequest is used to represent the request body to create or delete LTKs
+type LongTermKeyRequest struct {
+	Account     string `json:"account"`
+	Role        string `json:"role"`
+	IamUserName string `json:"iamUserName"`
+}
+
+// CreateLongTermKeyResponse is the response to the CLI client
+type CreateLongTermKeyResponse struct {
+	BaseResponse
+	CreateLongTermKey
+}
+
+// DeleteLongTermKey represents the response from API
+type DeleteLongTermKey struct {
+	AddedIAMUserToGroup bool `json:"addedIAMUserToGroup"`
+	PartialError        bool `json:"partialError"`
+}
+
+// DeleteLongTermKeyResponse is the response to the CLI client
+type DeleteLongTermKeyResponse struct {
+	BaseResponse
+	DeleteLongTermKey
 }
 
 // GetLongTermKeys gets the LTKs for an account
@@ -51,4 +87,94 @@ func (c *Client) GetLongTermKeys(accountId string, roleName string) (*GetLongTer
 	}
 
 	return cr, nil
+}
+
+// CreateLongTermKeys creates an LTK user for an account.
+// If no error is returned, then you will receive an appropriate success message.
+func (c *Client) CreateLongTermKey(accountId string, roleName string, accountAlias string, iamUsername string) (*CreateLongTermKeyResponse, error) {
+	log.Printf("[INFO] Creating long term key for: %s/%s - %s", accountId, roleName, accountAlias)
+
+	request := LongTermKeyRequest{
+		Account:     accountId + "/ALKS" + roleName + " - " + accountAlias,
+		Role:        roleName,
+		IamUserName: iamUsername,
+	}
+
+	reqBody, err := json.Marshal(struct{ LongTermKeyRequest }{request})
+
+	if err!= nil {
+		return nil, fmt.Errorf("error encoding LTK create JSON: %s", err)
+	}
+
+	req, err := c.NewRequest(reqBody, "POST", "/accessKeys")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	cr := new(CreateLongTermKeyResponse)
+	err = decodeBody(resp, &cr)
+
+	if err != nil {
+		if reqId := GetRequestID(resp); reqId != "" {
+			return nil, fmt.Errorf("error parsing CreateLongTermKeyResponse: [%s] %s", reqId, err)
+		}
+
+		return nil, fmt.Errorf("error parsing CreateLongTermKeyResponse: %s", err)
+	}
+
+	if cr.RequestFailed() {
+		return nil, fmt.Errorf("error creating long term key: [%s] %s", cr.BaseResponse.RequestID, strings.Join(cr.GetErrors(), ", "))
+	}
+
+	return cr, nil
+}
+
+// DeleteLongTermKeys deletes an LTK user for an account.
+// If no error is returned, then you will receive an appropriate success message.
+func (c *Client) DeleteLongTermKey(accountId string, roleName string, accountAlias string, iamUsername string) (*DeleteLongTermKeyResponse, error) {
+	log.Printf("[INFO] Deleting long term key user for: %s/%s - %s", accountId, roleName, accountAlias)
+
+	request := LongTermKeyRequest{
+		Account:     accountId + "/ALKS" + roleName + " - " + accountAlias,
+		Role:        roleName,
+		IamUserName: iamUsername,
+	}
+	reqBody, err := json.Marshal(struct{ LongTermKeyRequest }{request})
+
+	if err != nil {
+		return nil, fmt.Errorf("error encoding LTK delete JSON: %s", err)
+	}
+
+	req, err := c.NewRequest(reqBody, "DELETE", "/IAMUser")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	cr := new(DeleteLongTermKeyResponse)
+	err = decodeBody(resp, &cr)
+
+	if err != nil {
+		if reqId := GetRequestID(resp); reqId != "" {
+			return nil, fmt.Errorf("error parsing DeleteLongTermKeyResponse: [%s] %s", reqId, err)
+		}
+
+		return nil, fmt.Errorf("error parsing DeleteLongTermKeyResponse: %s", err)
+	}
+
+	if cr.RequestFailed() {
+		return nil, fmt.Errorf("error deleting long term key: [%s] %s", cr.BaseResponse.RequestID, strings.Join(cr.GetErrors(), ", "))
+	}
+
+	return cr, nil
+
 }
