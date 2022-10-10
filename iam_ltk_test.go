05027b8047fab78f1bd55783d4b4f04e52e5c8fb
Zack Elliott <elliottzack429@gmail.com>
2022-10-10 10:46:49 -0500

    in progress

--- iam_ltk_test.go
+++ iam_ltk_test.go
@@ -15,7 +15,7 @@ func (s *S) Test_GetLongTermKeys(c *C) {
 	c.Assert(resp.LongTermKeys, DeepEquals, []LongTermKey{{UserName: "bob", AccessKeyID: "verySecret", Status: "Active", CreateDate: "2020-04-20"}})
 }
 
-func (s *S) Test_GetLongTermKey(c *C) {
+func (s *S) Test_GetLongTermKeyNoTags(c *C) {
 	testServer.Response(202, nil, longTermKey)
 
 	resp, err := s.client.GetLongTermKey("bob")
@@ -24,12 +24,96 @@ func (s *S) Test_GetLongTermKey(c *C) {
 
 	c.Assert(err, IsNil)
 	c.Assert(resp.LongTermKey, DeepEquals, LongTermKey{})
+
 }
 
-func (s *S) Test_CreateLongTermKeys(c *C) {
-	testServer.Response(202, nil, createLongTermKeys)
+func (s *S) Test_GetLongTermKeyWithTags(c *C) {
+	testServer.Response(202, nil, longTermKeyWithTags)
 
-	resp, err := s.client.CreateLongTermKey("MY_USERNAME")
+	resp, err := s.client.GetLongTermKey("bob")
+
+	_ = testServer.WaitRequest()
+
+	c.Assert(err, IsNil)
+	c.Assert(resp.LongTermKey, DeepEquals, LongTermKey{})
+	c.Assert(resp.Tags[0].Key, Equals, "foo")
+	c.Assert(resp.Tags[0].Value, Equals, "bar")
+	c.Assert(resp.Tags[1].Key, Equals, "cloud")
+	c.Assert(resp.Tags[1].Value, Equals, "railway")
+}
+
+func (s *S) Test_CreateLongTermKeysNoIamUserName(c *C) {
+	testServer.Response(202, nil, createLongTermKeysNoTags)
+	tags := []Tag{
+		{
+			Key:   "foo",
+			Value: "bar",
+		},
+		{
+			Key:   "cloud",
+			Value: "railway",
+		},
+	}
+
+	options := CreateLongTermKeyOptions{
+		Tags: &tags,
+	}
+
+	resp, err := s.client.CreateLongTermKey(&options)
+
+	c.Assert(err, NotNil)
+	c.Assert(resp, IsNil)
+}
+
+func (s *S) Test_CreateLongTermKeysNoTags(c *C) {
+	testServer.Response(202, nil, createLongTermKeysNoTags)
+
+	username := "MY_USERNAME" 
+
+	options := CreateLongTermKeyOptions{
+		IamUserName: &username,
+	}
+
+	resp, err := s.client.CreateLongTermKey(&options)
+
+	c.Assert(err, IsNil)
+	c.Assert(resp.CreateLongTermKey, DeepEquals, CreateLongTermKey{
+		IAMUserName: "MY_USERNAME",
+		IAMUserArn:  "arn:aws:iam::012345678910:user/acct-managed/MY_USERNAME",
+		AccessKey:   "thisismykey",
+		SecretKey:   "secret/thisismysecret",
+	})
+	c.Assert(resp.BaseLongTermKeyResponse, DeepEquals, BaseLongTermKeyResponse{
+		Action:              "CREATE",
+		AddedIAMUserToGroup: true,
+		PartialError:        false,
+	})
+}
+
+func (s *S) Test_CreateLongTermKeysWithTags(c *C) {
+	testServer.Response(202, nil, createLongTermKeysWithTags)
+
+	username := "MY_USERNAME" 
+
+	tags := []Tag{
+		{
+			Key:   "foo",
+			Value: "bar",
+		},
+		{
+			Key:   "cloud",
+			Value: "railway",
+		},
+	}
+
+	options := CreateLongTermKeyOptions{
+		IamUserName: &username,
+		Tags: &tags,
+	}
+
+	resp, err := s.client.CreateLongTermKey(&options)
+
+	_ = testServer.WaitRequest()
 
 	c.Assert(err, IsNil)
 	c.Assert(resp.CreateLongTermKey, DeepEquals, CreateLongTermKey{
@@ -64,7 +148,7 @@ var deleteLongTermKeys = `
 }
 `
 
-var createLongTermKeys = `
+var createLongTermKeysNoTags = `
 {
     "account": "012345678910/ALKSAdmin - AccountAlias",
     "action": "CREATE",
@@ -74,6 +158,29 @@ var createLongTermKeys = `
     "partialError": false,
 	"accessKey": "thisismykey",
     "secretKey": "secret/thisismysecret"
+}
+`
+
+var createLongTermKeysWithTags = `
+{
+    "account": "012345678910/ALKSAdmin - AccountAlias",
+    "action": "CREATE",
+    "iamUserName": "MY_USERNAME",
+    "iamUserArn": "arn:aws:iam::012345678910:user/acct-managed/MY_USERNAME",
+    "addedIAMUserToGroup": true,
+    "partialError": false,
+	"accessKey": "thisismykey",
+    "secretKey": "secret/thisismysecret",
+	"tags": [
+        {
+            "key": "foo",
+            "value": "bar"
+        },
+        {
+            "key": "cloud",
+            "value": "railway"
+        }
+    ]
 }
 `
 
@@ -96,5 +203,24 @@ var longTermKey = `
 	"accessKeyId": "verySecret",
 	"status": "Active",
 	"createDate": "2020-04-20"
+}
+`
+
+var longTermKeyWithTags = `
+{
+	"userName": "bob",
+	"accessKeyId": "verySecret",
+	"status": "Active",
+	"createDate": "2020-04-20",
+	"tags": [
+        {
+            "key": "foo",
+            "value": "bar"
+        },
+        {
+            "key": "cloud",
+            "value": "railway"
+        }
+    ]
 }
 `
